6e05d9fce80f5cf1560ead66cf17386e6b61b52d
ryanjyost <ryost217@gmail.com>
2023-09-06 21:50:54 -0600

    fixing for public use

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ class PromptManager {
 
   constructor() {
     // const src = path.join(__dirname, 'prompts', "../..");
-    const src = path.join(__dirname, 'prompts', '../../..');
+    const src = path.join(__dirname, '../../../..', 'prompts');
     console.log('__dirname', __dirname, 'src', src);
     this.config = {
       source: src,
